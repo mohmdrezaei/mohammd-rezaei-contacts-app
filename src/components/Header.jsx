ad65fd234628c9afd1d364acc2ebad721a20a4c4
mohmdrezaei <mhwmdrz9@gmail.com>
2024-08-29 01:17:27 +0330

    Create Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+        <h1>Contact App</h1>
+        <p>Mohammad Rezaei | React</p>
+    </div>
+  )
+}
+
+export default Header
