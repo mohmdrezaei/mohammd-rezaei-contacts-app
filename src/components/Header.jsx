c2f571662332f9b88f05c4c627e823334e3a7502
mohmdrezaei <mhwmdrz9@gmail.com>
2024-08-29 19:27:14 +0330

    Change the header site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,21 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <div className={styles.container}>
-        <h1>Contact App</h1>
-        <p>Mohammad Rezaei | React</p>
+      <div className={styles.icon}>
+        <img src="./src/assets/logo.png" alt="" />
+        <p>Contact App</p>
+      </div>
+      <div className={styles.search}>
+        <img src="./src/assets/search.png" alt="" />
+        <input type="text" name="" id="" placeholder="Search" />
+      </div>
+
+      <div className={styles.addBtn}>
+        <img src="./src/assets/add.png" alt="" />
+        <p>Create contact</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
