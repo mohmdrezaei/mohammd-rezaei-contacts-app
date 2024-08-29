8074baf210cf126ab6ef285bba0cdd00b3a3aae1
mohmdrezaei <mhwmdrz9@gmail.com>
2024-08-30 01:25:33 +0330

    Dynamic profile image and Display the error to the user

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,21 +1,50 @@
 import React, { useState } from "react";
 import styles from "./ContactForm.module.css";
 function contactForm(props) {
-  const { setCurrentPage,contact , setCotact ,setContacts } = props;
+  const { setCurrentPage, contact, setCotact, setContacts } = props;
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  
   const changeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
     setCotact((contact) => ({ ...contact, [name]: value }));
   };
-  const addHandler =()=>{
-      setContacts(contacts => ([...contacts, contact]))
-      setCotact({
-        name: "",
-        email: "",
-        phone: "",
-      })
-      setCurrentPage("contactList")
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const validatePhone = (phone)  => phone.length >= 10; 
+  
+  const addHandler = () => {
+    const newErrors = {
+      name: !contact.name ? "Name is required." : "",
+      email: !contact.email || !validateEmail(contact.email) ? "Invalid email format." : "",
+      phone: !contact.phone || !validatePhone(contact.phone) ? "Invalid phone number." : "",
+    };
+    setErrors(newErrors);
+
+    if (newErrors.every((error) => error === "")) {
+    setContacts((contacts) => [...contacts, contact]);
+    setCotact({
+      name: "",
+      email: "",
+      phone: "",
+    });
+    setErrors({
+      name: "",
+      email: "",
+      phone: "",
+    })
+    console.log(errors)
+    setCurrentPage("contactList");
   }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.title}>
@@ -27,43 +56,54 @@ function contactForm(props) {
         <h1>Contact List</h1>
       </div>
       <div className={styles.profile}>
-        <img src="./src/assets/user-profile.png" alt="" />
+        <img
+          src={
+            contact.name
+              ? `https://ui-avatars.com/api/?name=${contact.name[0]}&length=1&background=random&size=262`
+              : "https://ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
+          }
+          alt="user-profile"
+        />
       </div>
 
       <div className={styles.form}>
-          <div className={styles.inputGroups}>
-            <img src="./src/assets/user-icon.png" alt="" />
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              value={contact.name}
-              onChange={changeHandler}
-            />
-          </div>
-          <div className={styles.inputGroups}>
-            <img src="./src/assets/email.png" alt="" />
-            <input
-              type="email"
-              placeholder="email"
-              name="email"
-              value={contact.email}
-              onChange={changeHandler}
-            />
-          </div>
-          <div className={styles.inputGroups}>
-            <img src="./src/assets/phone.png" alt="" />
-            <input
-              type="number"
-              placeholder="Phone number"
-              name="phone"
-              value={contact.phone}
-              onChange={changeHandler}
-            />
-          </div>
-          <button onClick={addHandler}>
-            Create
-          </button>
+        <div className={styles.inputGroups}>
+          <img src="./src/assets/user-icon.png" alt="" />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={contact.name}
+            onChange={changeHandler}
+          />
+             <span >{errors.name && errors.name}</span>
+        </div>
+     
+        <div className={styles.inputGroups}>
+          <img src="./src/assets/email.png" alt="" />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={contact.email}
+            onChange={changeHandler}
+          />
+           <span >{errors.email && errors.email}</span>
+        </div>
+        
+        <div className={styles.inputGroups}>
+          <img src="./src/assets/phone.png" alt="" />
+          <input
+            type="number"
+            placeholder="Phone number"
+            name="phone"
+            value={contact.phone}
+            onChange={changeHandler}
+          />
+           <span >{errors.phone && errors.phone}</span>
+        </div>
+        
+        <button onClick={addHandler}>Create</button>
       </div>
     </div>
   );
